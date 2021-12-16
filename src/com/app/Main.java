c25231299b0fc49d0248c2395951a0f68cbd4074
Van Le <70436051+iRok166@users.noreply.github.com>
2021-12-16 21:45:58 +0700

    Initial chat app

---
+++ src/com/app/Main.java
@@ -0,0 +1,13 @@
+package com.app;
+
+/**
+ * com.app
+ * Create by Le Nguyen Tu Van
+ * Date 12/16/2021 - 9:44 PM
+ * Description: ...
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
